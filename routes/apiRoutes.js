c6fb1fc97f564f53e795f20357379fd81ec50862
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-02-11 21:23:05 -0600

    apiroutes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const fs = require('fs');
+const notes = require('../db/db.json')
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,20 @@
+
 const express = require('express');
 
-const PORT = process.env.PORT || 3000;
-const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-app.use(express.urlencoded({ extended: true}));
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 
-app.use('/api', apiRoutes)
-app.use('/', htmlRoutes)
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API serber now on [ort ${PORT}!`);
-})
+    console.log(`API server now on port ${PORT}!`);
+});
