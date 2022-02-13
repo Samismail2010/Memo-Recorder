0f3e86ca5a16dfa5c520e3b0d6b41a9207c400ab
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-02-12 21:02:26 -0600

    complete api POST

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,22 @@
 const router = require('express').Router();
+const res = require('express/lib/response');
 const fs = require('fs');
-const notes = require('../db/db.json')
+let note = require('../db/db.json');
 
+
+//request to notes
+router.get('/notes', (req, res) => {
+    //reading db.son pull data as a JSON
+    note = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    return res.json(note)
+});
+
+router.post('/notes', (req, res) => {
+
+    let newNote = { title: req.body.title, text: req.body.text, id: Math.floor(Math.random()*1000)
+    }
+    note.push(newNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(note))
+    res.json(note);
+});
 module.exports = router;
